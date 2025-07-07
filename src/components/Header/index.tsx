b9a4527bba7a074b2219a66d2efebfeccac7ec0d
Felipe Paschoal <felipepaschoal.dev@gmail.com>
2025-07-07 14:32:30 -0300

    feat: adicionada biblioteca de ícones phosphor-react-native

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,14 @@
 import logoImg from '@assets/logo.png';
-import { Image, View } from 'react-native';
+import { CaretLeftIcon } from 'phosphor-react-native';
+import { Image, TouchableOpacity, View } from 'react-native';
+import { useTheme } from 'styled-components';
 
-export function Header() {
+type Props = {
+  showBackButton?: boolean;
+};
+
+export function Header({ showBackButton }: Props) {
+  const { WHITE } = useTheme().COLORS;
   return (
     <View
       style={{
@@ -11,6 +18,11 @@ export function Header() {
         justifyContent: 'center',
       }}
     >
+      {showBackButton && (
+        <TouchableOpacity style={{ flex: 1 }}>
+          <CaretLeftIcon color={WHITE} size={32} />
+        </TouchableOpacity>
+      )}
       <Image style={{ width: 46, height: 55 }} source={logoImg} />
     </View>
   );
