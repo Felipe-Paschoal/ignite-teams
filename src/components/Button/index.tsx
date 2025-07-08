a54699d38d286093b7bead6c8ca573bc74d94d05
Felipe Paschoal <felipepaschoal.dev@gmail.com>
2025-07-08 14:54:22 -0300

    feat: adicionado componente Button

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,41 @@
+import { TouchableOpacity, Text, TouchableOpacityProps } from 'react-native';
+import { useTheme } from 'styled-components';
+
+type ButtonTypeStyleProps = 'PRIMARY' | 'SECONDARY';
+
+type Props = TouchableOpacityProps & {
+  title: string;
+  type?: ButtonTypeStyleProps;
+};
+
+export function Button({ title, type = 'PRIMARY', ...rest }: Props) {
+  const theme = useTheme();
+
+  const backgroundColor =
+    type === 'PRIMARY' ? theme.COLORS.GREEN_700 : theme.COLORS.RED_DARK;
+
+  return (
+    <TouchableOpacity
+      {...rest}
+      style={{
+        flex: 1,
+        minHeight: 56,
+        maxHeight: 56,
+        backgroundColor,
+        borderRadius: 6,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Text
+        style={{
+          fontSize: theme.FONT_SIZE.MD,
+          color: theme.COLORS.WHITE,
+          fontFamily: theme.FONT_FAMILY.BOLD,
+        }}
+      >
+        {title}
+      </Text>
+    </TouchableOpacity>
+  );
+}
