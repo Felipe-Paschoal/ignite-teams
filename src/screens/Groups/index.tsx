94cf2a374b0cbc223464e1bab58a8ee1a11717c6
Felipe Paschoal <felipepaschoal.dev@gmail.com>
2025-07-10 16:52:59 -0300

    feat: implementado fetchGroup e CreateGroup nas telas

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -3,8 +3,9 @@ import { GroupCard } from '@components/GroupCard';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { ListEmpty } from '@components/ListEmpty';
-import { useNavigation } from '@react-navigation/native';
-import { useState } from 'react';
+import { useNavigation, useFocusEffect } from '@react-navigation/native';
+import { groupsGetAll } from '@storage/group/groupsGetAll';
+import { useEffect, useState, useCallback } from 'react';
 import { FlatList, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useTheme } from 'styled-components';
@@ -17,6 +18,27 @@ export function Groups() {
   function handleNewGroup() {
     navigation.navigate('new');
   }
+
+  async function fetchGroups() {
+    try {
+      const data = await groupsGetAll();
+
+      setGroups(data);
+    } catch (error) {}
+  }
+
+  function handleOpenGroup(group: string) {
+    navigation.navigate('players', {
+      group,
+    });
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchGroups();
+    }, [])
+  );
+
   return (
     <SafeAreaView
       style={{
@@ -30,7 +52,9 @@ export function Groups() {
       <FlatList
         data={groups}
         keyExtractor={(item) => item}
-        renderItem={({ item }) => <GroupCard title={item} />}
+        renderItem={({ item }) => (
+          <GroupCard onPress={() => handleOpenGroup(item)} title={item} />
+        )}
         contentContainerStyle={groups.length === 0 && { flex: 1 }}
         ListEmptyComponent={() => (
           <ListEmpty message="Que tal cadastrar a primeira turma?" />
